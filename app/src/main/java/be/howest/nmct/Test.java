d07acf3401f131aacc9732cda1602eff81bdfa71
stijn.walcarius@howest.be <stijnwalcarius@hotmail.com>
2015-01-30 00:58:43 +0100

    Initial commit

---
+++ app/src/main/java/be/howest/nmct/Test.java
@@ -0,0 +1,12 @@
+package be.howest.nmct;
+
+/**
+ * Created by Stijn on 30/01/2015.
+ */
+public class Test {
+
+    public static void main(String [] args){
+        System.out.println("even testen...");
+
+    }
+}
